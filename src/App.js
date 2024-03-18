afe7330ac3beff17e4356db4a9501faa41d777df
chaebeen <tnwl05200@gmail.com>
2024-03-18 23:55:20 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import {  Navbar, Container , Nav } from 'react-bootstrap'; //대문자로 된 컴포넌트 다 Import 해야 함
+import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
+ 
+  let [shop] = useState(data);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+        <Link to= '/'>notting-written</Link>
+          <Nav className="me-auto">
+            <Link to= '/detail'>outer</Link>
+            <Nav.Link href="#features">knit</Nav.Link>
+            <Nav.Link href="#pricing">bottom</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+
+      <Routes>
+        <Route path='/' element={
+        <>
+            <div className='main-bg'></div>
+            <div className='container'>
+              <div className='row'>
+            { shop.map(function(a, i){
+              return <Card shop = {shop[i]} i={i + 1} />
+            })}
+            </div>
+            </div>
+        </>
+      } />  
+        <Route path='/detail' element={<div>상세페이지</div>} />
+      </Routes>
+
     </div>
-  );
+      )
+    }
+
+
+function Card(props) {
+
+  return(
+    <div className='col-md-4'>
+    <img src={process.env.PUBLIC_URL + './img/outer' + props.i + '.jpeg'}
+      width= '50%'></img>
+      <h4>{props.shop.title}</h4>
+      <p>{props.shop.price}</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,19 @@
+let data = [
+    {
+        id : 0,
+        title : 'Harri mac coat (Black)',
+        price : 389000
+    },
+    {
+        id : 1,
+        title : 'Single wool jacket (Dark navy)',
+        price : 379000
+    },
+    {
+        id : 2,
+        title : 'Botti down half jacket (Black)',
+        price : 495000
+    }
+]
+
+export default data;
